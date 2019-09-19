adc787a3368c94fb896fd6270b242348f7eb96f3
Patrick Emond <emondpd@mcmaster.ca>
2019-09-19 17:32:42 -0400

    Adding in hotkeys

--- web/app/page/module.js
+++ web/app/page/module.js
@@ -113,8 +113,8 @@ define( function() {
 
   /* ######################################################################################################## */
   cenozo.providers.directive( 'cnPageRender', [
-    'CnPageModelFactory', '$q',
-    function( CnPageModelFactory, $q ) {
+    'CnPageModelFactory', '$q', '$document', '$transitions',
+    function( CnPageModelFactory, $q, $document, $transitions ) {
       return {
         templateUrl: module.getFileUrl( 'render.tpl.html' ),
         restrict: 'E',
@@ -122,6 +122,38 @@ define( function() {
         controller: function( $scope ) {
           $scope.isComplete = false;
           if( angular.isUndefined( $scope.model ) ) $scope.model = CnPageModelFactory.root;
+
+          // bind keypresses (first unbind to prevent duplicates)
+          $document.unbind( 'keydown.render' );
+          $document.bind( 'keydown.render', function( event ) {
+            var renderModel = $scope.model.renderModel;
+            if( 'render' == $scope.model.getActionFromState() &&
+                0 < renderModel.keyQuestionList.length &&
+                angular.isUndefined( event.target.type ) &&
+                96 <= event.which && event.which <= 105 ) {
+              var rank = event.which - 96;
+              var question = renderModel.keyQuestionList[renderModel.keyQuestionIndex];
+
+              // check if the key's rank is within the option list or the 2 dkna/refuse options
+              if( rank <= question.optionList.length ) {
+                var answer = question.optionList[rank-1];
+                renderModel.data[question.id][answer.id] = !renderModel.data[question.id][answer.id];
+                renderModel.setAnswer( question, answer );
+              } else if( rank == question.optionList.length + 1 ) {
+                renderModel.data[question.id].dkna = !renderModel.data[question.id].dkna;
+                renderModel.setAnswer( question, 'dkna' );
+              } else if( rank == question.optionList.length + 2 ) {
+                renderModel.data[question.id].refuse = !renderModel.data[question.id].refuse;
+                renderModel.setAnswer( question, 'refuse' );
+              }
+              $scope.$apply();
+
+              // advance to the next question, looping back to the first when we're at the end of the list
+              renderModel.keyQuestionIndex++;
+              if( renderModel.keyQuestionIndex >= renderModel.keyQuestionList.length ) renderModel.keyQuestionIndex = 0;
+            }
+          } );
+
           $q.all( [
             $scope.model.viewModel.onView(),
             $scope.model.renderModel.onLoad()
@@ -168,16 +200,32 @@ define( function() {
 
   /* ######################################################################################################## */
   cenozo.providers.factory( 'CnPageRenderFactory', [
-    'CnHttpFactory', '$state',
-    function( CnHttpFactory, $state ) {
+    'CnHttpFactory', '$state', '$document', '$transitions',
+    function( CnHttpFactory, $state, $document, $transitions ) {
       var object = function( parentModel ) {
         var self = this;
+
+        function setExclusiveAnswer( questionId, value ) {
+          // unselect all other values
+          for( var property in self.data[questionId] ) {
+            if( self.data[questionId].hasOwnProperty( property ) ) {
+              if( 'other' != property && value != property ) self.data[questionId][property] = false;
+            }
+          }
+
+          // unset the other value
+          if( angular.isDefined( self.data[questionId].other ) ) self.data[questionId].other = null;
+        }
 
         angular.extend( this, {
           parentModel: parentModel,
           questionList: [],
           data: {},
+          keyQuestionList: null,
+          keyQuestionIndex: null,
           onLoad: function() {
+            this.keyQuestionList = [];
+            this.keyQuestionIndex = 0;
             return CnHttpFactory.instance( {
               path: this.parentModel.getServiceResourcePath() + '/question'
             } ).query().then( function( response ) {
@@ -189,6 +237,7 @@ define( function() {
                 if( 'boolean' == question.type ) {
                   angular.extend( self.data[question.id], { yes: false, no: false } );
                 } else if( 'list' == question.type ) {
+                  self.keyQuestionList.push( question );
                   CnHttpFactory.instance( {
                     path: ['question', question.id, 'question_option' ].join( '/' ),
                     data: {
@@ -199,6 +248,7 @@ define( function() {
                     question.optionList = response.data;
                     question.optionList.forEach( function( option ) {
                       self.data[question.id][option.id] = false;
+                      if( 'OTHER' == option.value ) self.data[question.id].other = null;
                     } );
                   } );
                 }
@@ -206,23 +256,8 @@ define( function() {
             } );
           },
           setAnswer: function( question, value ) {
-            if( 'dkna' == value ) {
-              if( self.data[question.id].dkna ) {
-                // unselect all values
-                for( var property in self.data[question.id] ) {
-                  if( self.data[question.id].hasOwnProperty( property ) ) {
-                    if( 'dkna' != property ) self.data[question.id][property] = false;
-                  }
-                }
-              }
-            } else if( 'refuse' == value ) {
-              if( self.data[question.id].refuse ) {
-                for( var property in self.data[question.id] ) {
-                  if( self.data[question.id].hasOwnProperty( property ) ) {
-                    if( 'refuse' != property ) self.data[question.id][property] = false;
-                  }
-                }
-              }
+            if( 'dkna' == value || 'refuse' == value ) {
+              if( self.data[question.id][value] ) setExclusiveAnswer( question.id, value );
             } else {
               // handle each question type
               if( 'boolean' == question.type ) {
@@ -237,12 +272,7 @@ define( function() {
                 // unselect certain values if we're checking this option
                 if( self.data[question.id][value.id] ) {
                   if( value.exclusive ) {
-                    // unselect all other values
-                    for( var property in self.data[question.id] ) {
-                      if( self.data[question.id].hasOwnProperty( property ) ) {
-                        if( value.id != property ) self.data[question.id][property] = false;
-                      }
-                    }
+                    setExclusiveAnswer( question.id, value.id );
                   } else {
                     // unselect all no-answer and exclusive values
                     self.data[question.id].dkna = false;
@@ -251,6 +281,12 @@ define( function() {
                       self.data[question.id][option.id] = false;
                     } );
                   }
+                }
+
+                // handle the special circumstance when clicking the other option
+                if( 'OTHER' == value.value ) {
+                  if( self.data[question.id][value.id] ) document.getElementById( 'other' + value.id ).focus();
+                  else self.data[question.id].other = null;
                 }
               }
             }
