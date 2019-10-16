49b535756db606d88a2407511e088f0dfdccaa4b
Patrick Emond <emondpd@mcmaster.ca>
2019-10-16 12:08:51 -0400

    Making sure to update response last datetime whenever a patch is done

--- src/service/response/patch.class.php
+++ src/service/response/patch.class.php
@@ -13,6 +13,18 @@ class patch extends \cenozo\service\patch
   /**
    * Extend parent method
    */
+  public function setup()
+  {
+    parent::setup();
+
+    // update the last datetime anytime the response is changed
+    $db_response = $this->get_leaf_record();
+    $db_response->last_datetime = util::get_datetime_object();
+  }
+
+  /**
+   * Extend parent method
+   */
   public function execute()
   {
     parent::execute();
