1f4fdf1bd5cb2c53198674229f60c773fbaf0e9d
Patrick Emond <emondpd@mcmaster.ca>
2019-11-06 09:39:47 -0500

    Left-aligning page render button text

--- settings.ini.php
+++ settings.ini.php
@@ -13,7 +13,7 @@ global $SETTINGS;
 $SETTINGS['general']['application_name'] = 'pine';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
 $SETTINGS['general']['version'] = '2.5';
-$SETTINGS['general']['build'] = '2c50f48';
+$SETTINGS['general']['build'] = '31730f1';
 
 // the location of the application's internal path
 $SETTINGS['path']['APPLICATION'] = str_replace( '/settings.ini.php', '', __FILE__ );
