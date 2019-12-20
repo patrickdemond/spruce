8437398c1fa70c7bc26758af09a00fd4d29bca7c
Patrick Emond <emondpd@mcmaster.ca>
2019-12-20 09:43:42 -0500

    Data from Opal need to be utf8 encoded

--- src/database/attribute.class.php
+++ src/database/attribute.class.php
@@ -16,8 +16,10 @@ class attribute extends \cenozo\database\record
   public function get_participant_value( $db_participant )
   {
     $data_manager = lib::create( 'business\data_manager' );
-    return 0 === strpos( $this->code, 'participant.' ) ?
+    return util::utf8_encode(
+      0 === strpos( $this->code, 'participant.' ) ?
       $data_manager->get_participant_value( $db_participant, $this->code ) :
-      $data_manager->get_value( $this->code );
+      $data_manager->get_value( $this->code )
+    );
   }
 }
