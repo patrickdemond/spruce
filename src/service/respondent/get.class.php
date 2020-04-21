f538f7c4758bbbba82d088f1d4f0f9909cce21db
Patrick Emond <emondpd@mcmaster.ca>
2020-04-21 11:07:44 -0400

    Fixing bug in last commit

--- src/service/respondent/get.class.php
+++ src/service/respondent/get.class.php
@@ -26,34 +26,41 @@ class get extends \cenozo\service\get
       $db_response = $db_respondent->get_current_response();
 
       // make sure there is a response
-      if( is_null( $db_response ) || is_null( $db_response->start_datetime ) )
+      if( is_null( $db_response ) )
       {
         // always create the first response
         $create_new_response = true;
       }
       else if( !is_null( $db_qnaire->repeated ) )
       {
-        // create a new response if the repeat span has passed since the last response
-        $diff = $db_response->start_datetime->diff( util::get_datetime_object() );
-        if( 'hour' == $db_qnaire->repeated )
+        if( is_null( $db_response->start_datetime ) )
         {
-          // count hours
-          $hours = 24 * $diff->days + $diff->h;
-          $create_new_response = $hours >= $db_qnaire->repeat_offset;
+          $create_new_response = true;
         }
-        else if( 'day' == $db_qnaire->repeated )
+        else
         {
-          $create_new_response = $diff->days >= $db_qnaire->repeat_offset;
-        }
-        else if( 'week' == $db_qnaire->repeated )
-        {
-          $create_new_response = $diff->days >= ( 7 * $db_qnaire->repeat_offset );
-        }
-        else if( 'month' == $db_qnaire->repeated )
-        {
-          // count months
-          $months = 12 * $diff->y + $diff->m;
-          $create_new_response = $months >= $db_qnaire->repeat_offset;
+          // create a new response if the repeat span has passed since the last response
+          $diff = $db_response->start_datetime->diff( util::get_datetime_object() );
+          if( 'hour' == $db_qnaire->repeated )
+          {
+            // count hours
+            $hours = 24 * $diff->days + $diff->h;
+            $create_new_response = $hours >= $db_qnaire->repeat_offset;
+          }
+          else if( 'day' == $db_qnaire->repeated )
+          {
+            $create_new_response = $diff->days >= $db_qnaire->repeat_offset;
+          }
+          else if( 'week' == $db_qnaire->repeated )
+          {
+            $create_new_response = $diff->days >= ( 7 * $db_qnaire->repeat_offset );
+          }
+          else if( 'month' == $db_qnaire->repeated )
+          {
+            // count months
+            $months = 12 * $diff->y + $diff->m;
+            $create_new_response = $months >= $db_qnaire->repeat_offset;
+          }
         }
       }
 
