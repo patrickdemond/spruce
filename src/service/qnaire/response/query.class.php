1beb9ec6f2dfccf021c263c9ca8daae53f93dea2
Patrick Emond <emondpd@mcmaster.ca>
2020-07-15 14:59:50 -0400

    Adding CSV export of response data as new end-point

---
+++ src/service/qnaire/response/query.class.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * query.class.php
+ * 
+ * @author Patrick Emond <emondpd@mcmaster.ca>
+ */
+
+namespace pine\service\qnaire\response;
+use cenozo\lib, cenozo\log, pine\util;
+
+class query extends \cenozo\service\query
+{
+  /**
+   * Extend parent method
+   */
+  public function get_leaf_parent_relationship()
+  {
+    $relationship_class_name = lib::get_class_name( 'database\relationship' );
+    return $relationship_class_name::MANY_TO_MANY;
+  }
+
+  /**
+   * Extend parent method
+   */
+  protected function get_record_count()
+  {
+    // count all responses belonging to the parent qnaire
+    $response_class_name = lib::get_class_name( 'database\response' );
+    $modifier = clone $this->modifier;
+    $modifier->where( 'respondent.qnaire_id', '=', $this->get_parent_record()->id );
+    return $response_class_name::count( $modifier );
+  }
+
+  /**
+   * Extend parent method
+   */
+  protected function get_record_list()
+  {
+    // if exporting data then we need the qnaire class to generate it for us
+    if( $this->get_argument( 'export', false ) )
+    {
+      $modifier = lib::create( 'database\modifier');
+      $modifier->limit( $this->modifier->get_limit() );
+      $modifier->offset( $this->modifier->get_offset() );
+
+      $response_data = $this->get_parent_record()->get_response_data( $modifier );
+      $data = $response_data['data'];
+      foreach( $data as $index => $row ) $data[$index] = array_combine( $response_data['header'], $data[$index] );
+      return $data;
+    }
+    else
+    {
+      // list all responses belonging to the parent qnaire
+      $response_class_name = lib::get_class_name( 'database\response' );
+      $modifier = clone $this->modifier;
+      $modifier->where( 'respondent.qnaire_id', '=', $this->get_parent_record()->id );
+
+      return $response_class_name::select( $this->select, $modifier );
+    }
+  }
+}
