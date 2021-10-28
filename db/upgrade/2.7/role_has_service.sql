08dab13656a021fa6088b4a0908d6ab0b8037be3
Patrick Emond <emondpd@mcmaster.ca>
2021-10-28 17:04:24 -0400

    Fixing service access bug

--- db/upgrade/2.7/role_has_service.sql
+++ db/upgrade/2.7/role_has_service.sql
@@ -66,6 +66,21 @@ CREATE PROCEDURE patch_role_has_service()
     EXECUTE statement;
     DEALLOCATE PREPARE statement;
 
+    -- respondent
+    SET @sql = CONCAT(
+      "INSERT IGNORE INTO role_has_service( role_id, service_id ) ",
+      "SELECT role.id, service.id ",
+      "FROM ", @cenozo, ".role, service ",
+      "WHERE role.name = 'respondent' ",
+      "AND service.subject IN( 'address', 'participant' ) ",
+      "AND service.method = 'GET' ",
+      "AND service.resource = true ",
+      "AND service.restricted = 1"
+    );
+    PREPARE statement FROM @sql;
+    EXECUTE statement;
+    DEALLOCATE PREPARE statement;
+
   END //
 DELIMITER ;
 
