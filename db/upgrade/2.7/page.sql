dabd1d91b2e8cc0cbd3503b17fa03e4ff40dcb97
Patrick Emond <emondpd@mcmaster.ca>
2021-11-09 10:24:40 -0500

    Fixing bug in page triggers

--- db/upgrade/2.7/page.sql
+++ db/upgrade/2.7/page.sql
@@ -16,13 +16,15 @@ BEGIN
   WHERE module.id = NEW.module_id;
 
   -- update the qnaire's total number of pages
+  SELECT qnaire_id INTO @qnaire_id FROM module WHERE id = NEW.module_id;
+
   SELECT COUNT(*) INTO @pages
   FROM qnaire
   JOIN module ON qnaire.id = module.qnaire_id
   JOIN page ON module.id = page.module_id
-  WHERE qnaire.id = ( SELECT qnaire_id FROM module WHERE id = NEW.module_id );
+  WHERE qnaire.id = @qnaire_id;
 
-  UPDATE qnaire SET total_pages = @pages;
+  UPDATE qnaire SET total_pages = @pages WHERE id = @qnaire_id;
 
 END$$
 
@@ -30,13 +32,15 @@ DROP TRIGGER IF EXISTS page_AFTER_DELETE$$
 CREATE TRIGGER page_AFTER_DELETE AFTER DELETE ON page FOR EACH ROW
 BEGIN
   -- update the qnaire's total number of pages
+  SELECT qnaire_id INTO @qnaire_id FROM module WHERE id = OLD.module_id;
+
   SELECT IF( page.id IS NULL, 0, COUNT(*) ) INTO @pages
   FROM qnaire
   LEFT JOIN module ON qnaire.id = module.qnaire_id
   LEFT JOIN page ON module.id = page.module_id
-  WHERE qnaire.id = ( SELECT qnaire_id FROM module WHERE id = OLD.module_id );
+  WHERE qnaire.id = @qnaire_id;
 
-  UPDATE qnaire SET total_pages = @pages;
+  UPDATE qnaire SET total_pages = @pages WHERE id = @qnaire_id;
 END$$
 
 DELIMITER ;
