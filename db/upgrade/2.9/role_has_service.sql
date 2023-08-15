82ce13f05a0ff2ff1e44d6b5925e09cbcd1e98be
Patrick Emond <emondpd@mcmaster.ca>
2023-08-15 13:39:06 -0400

    Bug: machine couldn't export respondent data

--- db/upgrade/2.9/role_has_service.sql
+++ db/upgrade/2.9/role_has_service.sql
@@ -36,6 +36,19 @@ CREATE PROCEDURE patch_role_has_service()
     EXECUTE statement;
     DEALLOCATE PREPARE statement;
 
+    SET @sql = CONCAT(
+      "INSERT IGNORE INTO role_has_service( role_id, service_id ) ",
+      "SELECT role.id, service.id ",
+      "FROM ", @cenozo, ".role, service ",
+      "WHERE role.name = 'machine' ",
+      "AND service.subject = 'respondent' ",
+      "AND service.method = 'POST' ",
+      "AND service.restricted = 1"
+    );
+    PREPARE statement FROM @sql;
+    EXECUTE statement;
+    DEALLOCATE PREPARE statement;
+
   END //
 DELIMITER ;
 
