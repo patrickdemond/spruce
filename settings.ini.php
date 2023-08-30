61d45ed27805df1543bf1f10e63e99bc6321f32d
Patrick Emond <emondpd@mcmaster.ca>
2023-08-30 15:13:28 -0400

    Bug: making reverse compatible with old versions of Firefox
    As it turns out Firefox didn't support the "s" RegExp flag until 2020,
    but since we need to support versions dating back to 2017 the codehas
    been changed to do multi-line . matching without using the "s" flag.
    Specifically, the string's newlines are converted to string, the RegExp
    is run, then the newlines are converted back.

--- settings.ini.php
+++ settings.ini.php
@@ -13,7 +13,7 @@ global $SETTINGS;
 $SETTINGS['general']['application_name'] = 'pine';
 $SETTINGS['general']['instance_name'] = $SETTINGS['general']['application_name'];
 $SETTINGS['general']['version'] = '2.9';
-$SETTINGS['general']['build'] = '9254524';
+$SETTINGS['general']['build'] = 'fd5d6c9';
 
 // the default maximum number of seconds that a page should take to complete
 $SETTINGS['general']['default_page_max_time'] = 60;
