d0e30ab3999053a4f53f38a82a3ca0a11d1b6a3f
Patrick Emond <emondpd@mcmaster.ca>
2024-10-16 14:22:08 -0400

    Tweak: removing old LDAP references and defunct code

--- web/index.php
+++ web/index.php
@@ -3,9 +3,6 @@
  * Main web script which drives the application
  */
 
-namespace pine;
-use cenozo\lib, cenozo\log, pine\util;
-
 if( !array_key_exists( 'REDIRECT_URL', $_SERVER ) ||
     0 == preg_match( '#/app/[^/]+/[^/]+.extend(\.min)?.js#', $_SERVER['REDIRECT_URL'] ) )
 {
